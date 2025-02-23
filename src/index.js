b5c67824b7f395cb84d4b3adf835f3c4426e7731
BinhAn14 <an100277@donga.edu.vn>
2025-02-23 19:13:15 +0700

    up

--- src/index.js
+++ src/index.js
@@ -1,87 +1,94 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-var methodOverride = require('method-override')
-const handlebars = require('express-handlebars')
-const session = require('express-session')
-const MongoDBStore = require('connect-mongodb-session')(session)
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+var methodOverride = require("method-override");
+const handlebars = require("express-handlebars");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
-const flash = require('connect-flash')
+const flash = require("connect-flash");
 
-const SortMiddleware = require('./app/middlewares/sortMiddleware')
+const SortMiddleware = require("./app/middlewares/sortMiddleware");
 
-const User = require('./app/models/User');
-const route = require('./routes') // tự nạp file index
-const db = require('./config/db')
+const User = require("./app/models/User");
+const route = require("./routes"); // tự nạp file index
+const db = require("./config/db");
 
-const app = express()
+const app = express();
 
 // Connect to DB
 db.connect()
-  .then(result => {
-    const port = 3000
+  .then((result) => {
+    const port = 3000;
     app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`)
-    })
+      console.log(`Example app listening on port ${port}`);
+    });
   })
-  .catch()
+  .catch();
 
 const store = new MongoDBStore({
-  uri: 'mongodb://127.0.0.1:27017/ogranic',
-  collection: 'sessions'
-})
+  uri: "mongodb+srv://an100277:AeeReISLciRp5Aqf@binhan.awftk.mongodb.net/flutterDBBBB?retryWrites=true&w=majority&appName=Cluster0",
+  collection: "sessions",
+});
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 
-app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store }))
-
-
-app.use(flash())
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
-    return next()
+    return next();
   }
   User.findById(req.session.user._id)
-    .then(user => {
+    .then((user) => {
       req.user = user;
-      next()
+      next();
     })
-    .catch()
-})
+    .catch();
+});
 
 app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn
-  res.locals.isAuthenticatedAdmin = req.session.role - 2
-  res.locals.user = req.session.user
-  next()
-})
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.isAuthenticatedAdmin = req.session.role - 2;
+  res.locals.user = req.session.user;
+  next();
+});
 
-app.use(express.urlencoded(
-  {
-    extended: true
-  }
-));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 
 //Custom middlewares
-app.use(SortMiddleware)
+app.use(SortMiddleware);
 
 // HTTP logger
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
 // Template engine
-app.engine('hbs', handlebars.engine({
-  extname: '.hbs',
-  helpers: require('./helpers/handlebars')
-}))
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+    helpers: require("./helpers/handlebars"),
+  })
+);
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources', 'views'))
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources", "views"));
 
 // Rotes init
 route(app);
-
