31af0e7dda8a7823ff9323cf6ff18f788fd8afb9
BinhAn14 <an100277@donga.edu.vn>
2025-02-24 11:37:07 +0700

    up

--- src/index.js
+++ src/index.js
@@ -32,6 +32,7 @@ const store = new MongoDBStore({
 });
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static(path.join(__dirname, "public", "uploads")));
 
 app.use(
   session({
