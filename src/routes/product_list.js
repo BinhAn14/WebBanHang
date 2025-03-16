10ce2498d3a3002fed31a30fe4d8dde612af3f51
BinhAn14 <an100277@donga.edu.vn>
2025-03-16 11:05:37 +0700

    up

--- src/routes/product_list.js
+++ src/routes/product_list.js
@@ -1,15 +1,18 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const product_listController = require('../app/controllers/Product_listController')
+const product_listController = require("../app/controllers/Product_listController");
 
-router.get('/rau-cu', product_listController.show_raucu)
-router.get('/sinh-to', product_listController.show_sinhto)
-router.get('/thuc-pham-tuoi-song', product_listController.show_thucphamtuoisong)
-router.get('/hoa-qua', product_listController.show_hoaqua)
-router.get('/cac-loai-hat', product_listController.show_cacloaihat)
-router.get('/gia-vi', product_listController.show_giavi)
-router.get('/ngu-coc', product_listController.show_ngucoc)
-router.get('/sieu-thuc-pham', product_listController.show_sieuthucpham)
-
+router.get("/rau-cu", product_listController.show_raucu);
+router.get("/sinh-to", product_listController.show_sinhto);
+router.get(
+  "/thuc-pham-tuoi-song",
+  product_listController.show_thucphamtuoisong
+);
+router.get("/hoa-qua", product_listController.show_hoaqua);
+router.get("/cac-loai-hat", product_listController.show_cacloaihat);
+router.get("/gia-vi", product_listController.show_giavi);
+router.get("/ngu-coc", product_listController.show_ngucoc);
+router.get("/sieu-thuc-pham", product_listController.show_sieuthucpham);
+router.get("/api/products", productListController.getAllProducts);
 module.exports = router;
